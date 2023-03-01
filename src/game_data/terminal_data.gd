003f1d83ea0eb97cc578d979aaf8fe5b04c3657c
Julia Grill <jusogrill@gmail.com>
2023-03-01 17:27:59 +0100

    Add offset to intro text

--- src/game_data/terminal_data.gd
+++ src/game_data/terminal_data.gd
@@ -1,13 +1,13 @@
 extends Node
 
 const INTRO_DIALOGUE := [
-	">> Hello ... nice to see meet you. I'm guessing this is your first day? [i]Press ENTER or RETURN to continue[/i]",
+	"[offset x=10]>> Hello ... nice to see meet you. I'm guessing this is your first day? [i]Press ENTER or RETURN to continue[/i]",
 	"\n>> Either way: I'm Bob, your new partner.",
 	"\n>> Your job is to support the company with defending against attacks on our system. You'll be using this computer as your working device.",
-	str("\n> We've been regularly receiving threats ever since our CEO tweeted about a random celebrity. Don't ask me, I can't understand human emotions.",
+	str("\n>> We've been regularly receiving threats ever since our CEO tweeted about a random celebrity. Don't ask me, I can't understand human emotions.",
 		"Such a waste of time when you could be working.\nAnyway, I'll be here to support you and lend you my expertise and knowledge.",
 		"You'll be focusing on different types of threats on different days. Unfortunately I don't have a body yet so I'll be stuck in this terminal for the time being."),
-	"\n> Customers can store their data on our servers for free. Funnily enough, People think they're not the product. Funny, I know.",
+	"\n>> Customers can store their data on our servers for free. Funnily enough, People think they're not the product. Funny, I know.",
 	str("\n>> The terminal acts as the main interaction point between you and me.",
 		"Interacting with the terminal is straight forward. Enter any of the available commands to trigger certain actions. ",
 		"The commands might seem a bit meta sometimes, but trust me - it's good that they are. ",
@@ -15,7 +15,7 @@ const INTRO_DIALOGUE := [
 	str("\n>> You must complete challenges to defend our systems. These challenges share similarities with traditional minigames but don't be fooled: The system relies on your good performance."),
 	str("\n>> Your current tasks are displayed on the bottom right corner of the screen.",
 	"Entering [i]BACKSTORY[/i] should give you an idea of the state of our current system.",
-	"Today will start relatively slow. Your goal is to get acquainted with the system and play around.")
+	"Today will start relatively slow. Your goal is to get acquainted with the system and play around.[/offset]")
 ]
 
 const SUPPORTED_COMMANDS := {
