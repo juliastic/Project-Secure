53da88815da874335a3bea5eea5c229bc2ddfff1
Julia Grill <jusogrill@gmail.com>
2023-03-22 19:12:34 +0100

    Add link to survey at the end of game

--- src/level_finished/level_finished.gd
+++ src/level_finished/level_finished.gd
@@ -5,37 +5,39 @@ signal level_reset_triggered()
 var current_game_over = false
 var animation_complete = false
 
-func _ready():
+func _ready() -> void:
 	self.hide()
 	self.set_modulate(lerp(get_modulate(), Color(1, 1, 1, 0), 1))
 
 
 func _input(event) -> void:
-	if event is InputEventMouse and event.is_pressed() and animation_complete:
-		if current_game_over:
-			GameProgress.reset_level()
-			self.emit_signal("level_reset_triggered")
-		else:
-			GameProgress.complete_level()
-			var network = get_parent().get_node("Network")
-			network.hide()
-			if GameProgress.level == GameProgress.Level.RANSOMWARE:
-				network.get_node("RansomwareRequestMiniGame").queue_free()
-			elif GameProgress.level == GameProgress.Level.DDoS:
-				network.get_node("DDoSInvaderMiniGame").queue_free()
-			elif GameProgress.level == GameProgress.Level.SOCIAL_ENGINEERING:
-				network.get_node("SocialEngineeringMiniGame").queue_free()
-			elif GameProgress.level == GameProgress.Level.EoP:
-				GameProgress.reset_game()
-				get_tree().paused = false
-				if get_tree().change_scene("res://src/main/Main.tscn") != OK:
-					print("Couldn't switch to scene Main Scene")
-				return
-		$AnimationPlayer.play("Fade")
-		get_tree().paused = false
-		yield(get_node("AnimationPlayer"), "animation_finished")
-		self.hide()
-		animation_complete = false
+	if not event is InputEventMouse or not event.is_pressed() or not animation_complete:
+		return
+	if current_game_over:
+		GameProgress.reset_level()
+		self.emit_signal("level_reset_triggered")
+	else:
+		GameProgress.complete_level()
+		var network = get_parent().get_node("Network")
+		network.hide()
+		if GameProgress.level == GameProgress.Level.RANSOMWARE:
+			network.get_node("RansomwareRequestMiniGame").queue_free()
+		elif GameProgress.level == GameProgress.Level.DDoS:
+			network.get_node("DDoSInvaderMiniGame").queue_free()
+		elif GameProgress.level == GameProgress.Level.SOCIAL_ENGINEERING:
+			network.get_node("SocialEngineeringMiniGame").queue_free()
+		elif GameProgress.level == GameProgress.Level.EoP:
+			OS.shell_open("https://pads.c3w.at/form/#/2/form/view/YnUksqmPnvPJ-STTnNqTM3A+uvkrbq24ZbCQQ0Iucq8/")
+			GameProgress.reset_game()
+			get_tree().paused = false
+			if get_tree().change_scene("res://src/main/Main.tscn") != OK:
+				print("Couldn't switch to scene Main Scene")
+			return
+	$AnimationPlayer.play("Fade")
+	get_tree().paused = false
+	yield(get_node("AnimationPlayer"), "animation_finished")
+	self.hide()
+	animation_complete = false
 
 
 func _on_Desktop_level_finished_triggered(game_over) -> void:
@@ -45,14 +47,14 @@ func _on_Desktop_level_finished_triggered(game_over) -> void:
 		$Display.bbcode_text = "[center]Sorry - try a bit harder next time :)[/center]"
 	else:
 		if GameProgress.level == GameProgress.Level.EoP:
-			$Display.bbcode_text = "[center]THANK YOU SO MUCH!\nYou've saved our system![/center]"
+			$Display.bbcode_text = "[center]\nTHANK YOU SO MUCH!\nYou've saved our system!\n\nPress anywhere to be forwarded to the evaluation and return to the main menu.[/center]"
 			$BrokenCup.show()
 		elif GameProgress.level == GameProgress.Level.SOCIAL_ENGINEERING:
-			$Display.bbcode_text = str("[center]You've scored ", GameProgress.level_score[GameProgress.level], " points!\nMore importantly: We finally know what the attacker is up to. FIND THEM[/center]")
+			$Display.bbcode_text = str("[center]\nYou've scored ", GameProgress.level_score[GameProgress.level], " points!\nMore importantly: We finally know what the attacker is up to. FIND THEM[/center]")
 		elif GameProgress.level == GameProgress.Level.RANSOMWARE:
-			$Display.bbcode_text = str("[center]You've scored ", GameProgress.level_score[GameProgress.level], " points!\nInteresting things that are going on in the honeypot ...[/center]")
+			$Display.bbcode_text = str("[center]\nYou've scored ", GameProgress.level_score[GameProgress.level], " points!\nInteresting things that are going on in the honeypot ...[/center]")
 		elif GameProgress.level == GameProgress.Level.DDoS:
-			$Display.bbcode_text = str("[center]You've scored ", GameProgress.level_score[GameProgress.level], " points!\nYou showed these cups! But wait ...[/center]")
+			$Display.bbcode_text = str("[center]\nYou've scored ", GameProgress.level_score[GameProgress.level], " points!\nYou showed these cups! But wait ...[/center]")
 	self.show()
 	$AnimationPlayer.play_backwards("Fade")
 	yield(get_node("AnimationPlayer"), "animation_finished")
